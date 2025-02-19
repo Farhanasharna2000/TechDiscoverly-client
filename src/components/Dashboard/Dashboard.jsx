00f1a7cb9a4d50c0ade108ce47f9e93284641bcb
Farhanasharna2000 <farhanasharna22@gmail.com>
2025-02-19 17:15:34 +0600

    update

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -6,10 +6,16 @@ import { RiCouponFill } from "react-icons/ri";
 import { NavLink, Outlet } from "react-router-dom";
 import useRole from "../../hooks/useRole";
 import { GrOverview } from "react-icons/gr";
+import { FiMoon, FiSun } from "react-icons/fi";
+import { useTheme } from "../../hooks/UseTheme";
 
 const Dashboard = () => {
   const [role] = useRole();
+  const { theme, setTheme } = useTheme();
 
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
   return (
     <div className="flex bg-[#FFF8E6]/30">
       {/* Dashboard sidebar */}
@@ -96,11 +102,21 @@ const Dashboard = () => {
       {/* Dashboard content */}
       <div className="flex-1  ml-[35%] md:ml-[20%]">
         {/* Navbar at the top of the content area */}
-        <div className="bg-white p-4 shadow-lg ml-[20%] md:px-8 fixed top-0 left-0 right-0 z-10">
+        <div className="bg-white px-4 py-2 shadow-lg ml-[20%] md:px-8 fixed top-0 left-0 right-0 z-10">
           <div className="flex justify-end items-center">
             <NavLink to="/dashboard/my-profile" className="space-x-2">
               <FaUser className="text-4xl rounded-full border p-2" />
             </NavLink>
+            <button
+            onClick={toggleTheme}
+            className="btn btn-ghost rounded-full px-2 ml-3"
+          >
+            {theme === 'light' ? (
+              <FiMoon className="text-2xl text-[#8D0B41]" />
+            ) : (
+              <FiSun className="text-2xl text-yellow-300" />
+            )}
+          </button>
           </div>
         </div>
 
--- src/main.jsx
+++ src/main.jsx
@@ -7,17 +7,20 @@ import { HelmetProvider } from 'react-helmet-async'
 import AuthProvider from './providers/AuthProvider'
 import {  RouterProvider } from 'react-router-dom'
 import { router } from './routes/Routes'
+import { ThemeProvider } from './hooks/UseTheme'
 // Create a client
 const queryClient = new QueryClient()
 createRoot(document.getElementById('root')).render(
   <StrictMode>
  <AuthProvider>
+ <ThemeProvider>
       <HelmetProvider>
         <QueryClientProvider client={queryClient}>
           <RouterProvider router={router} />
         </QueryClientProvider>
         <Toaster position='top-right' reverseOrder={false} />
       </HelmetProvider>
+ </ThemeProvider>
     </AuthProvider>
   </StrictMode>
 )
